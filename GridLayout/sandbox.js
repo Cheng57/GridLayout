ae03af08cf899be52900e04fd5fd4c7a8a0cba52
Cheng Wu <cwu57cn@gmail.com>
2024-01-15 15:21:43 -0600

    Create test file

---
+++ GridLayout/sandbox.js
@@ -0,0 +1,15 @@
+const paras = document.querySelector('p');
+console.log(paras);
+
+
+const parass = document.getElementsByClassName('first');
+console.log(parass);
+
+// paras.forEach(function(para){
+//     if (para.innerText.includes('error')){
+//         para.classList.add('error');
+//     }
+//     if (para.innerText.includes('success')){
+//         para.classList.add('success');
+//     }
+// });
